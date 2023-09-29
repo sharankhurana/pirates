d73fb70c0aea8d0bc57701f4b1cc9f217c46c75d
sharankhurana <139131105+sharankhurana@users.noreply.github.com>
2023-09-29 16:25:56 +0530

    Add files via upload

---
+++ js/Boat.js
@@ -0,0 +1,28 @@
+class Boat {
+
+  constructor(x, y, width, height,boatPos) {
+    var options = {
+     restitution:0.8,
+     friction:1.0,
+     density:1.0
+  
+    };
+    this.width = width;
+    this.height = height;
+    this.body = Bodies.rectangle(x, y, this.width, this.height, options);
+    this.boatPosition= boatPos;
+    this.image=loadImage("assets/boat.png")
+    
+    World.add(world, this.body);
+  }
+  display() {
+    var pos = this.body.position;
+    var angle = this.body.angle
+    push();
+    translate(pos.x, pos.y);
+    fill("brown");
+    imageMode(CENTER);
+    image(this.image, 0, this.boatPosition, this.width, this.height);
+    pop();
+  }
+}
